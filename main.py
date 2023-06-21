1dfd840e0fa8676eb92213e2de01907181422a70
JangSeHun <westreed@naver.com>
2023-06-21 19:45:25 +0900

    refactor : main code

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ from s3_llm_prompts_format import init as s3_init
 from s4_llm_chain import init as s4_init
 from s5_agent import init as s5_init
 
+OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
+SERPAPI_API_KEY = os.environ["SERPAPI_API_KEY"]
+
 if __name__ == "__main__":
-    OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
-    SERPAPI_API_KEY = os.environ["SERPAPI_API_KEY"]
     # s1_init(OPENAI_API_KEY)
     # s2_init(OPENAI_API_KEY)
     # s3_init(OPENAI_API_KEY)
