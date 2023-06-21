80f430afcafaf4ce3ea4ff99888b5ef357b636c7
JangSeHun <westreed@naver.com>
2023-06-21 22:42:05 +0900

    refactor : optional os code

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
-import os
+import os, sys
 from s1_llm_predict import init as s1_init
 from s2_chat_predict import init as s2_init
 from s3_llm_prompts_format import init as s3_init
 from s4_llm_chain import init as s4_init
 from s5_agent import init as s5_init
 
-OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
-SERPAPI_API_KEY = os.environ["SERPAPI_API_KEY"]
+if sys.platform == 'darwin':
+    # MacOS
+    OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
+    SERPAPI_API_KEY = os.environ["SERPAPI_API_KEY"]
+elif sys.platform == 'win32':
+    # Windows
+    OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+    SERPAPI_API_KEY = os.getenv("SERPAPI_API_KEY")
 
 if __name__ == "__main__":
-    # s1_init(OPENAI_API_KEY)
+    s1_init(OPENAI_API_KEY)
     # s2_init(OPENAI_API_KEY)
     # s3_init(OPENAI_API_KEY)
     # s4_init(OPENAI_API_KEY)
-    s5_init(OPENAI_API_KEY, SERPAPI_API_KEY)
+    # s5_init(OPENAI_API_KEY, SERPAPI_API_KEY)
