ce90617952bd9835814e427716e3ce4d1ca90a48
JangSeHun <westreed@naver.com>
2023-07-03 00:09:17 +0900

    feat: MVP스러운 예제 만들기
    생성된 질문 텍스트를 파싱하는 함수 추가.

--- mvp/util.py
+++ mvp/util.py
@@ -1,9 +1,10 @@
 import json
+from typing import *
 
 INPUT_FILE_NAME = "input"
 
 
-def input_user_info():
+def input_user_info() -> Union[Dict, bool]:
     try:
         with open(f"{INPUT_FILE_NAME}.json", "r", encoding="UTF-8") as f:
             input_data = json.load(f)
@@ -21,5 +22,18 @@ def input_user_info():
             )
         print("input.json을 열고, 정보를 작성하세요.")
         return False
-    
+
     return input_data
+
+
+def split_question(question: str) -> List:
+    question_list = question.replace("\n\n", "\n").split("\n")
+
+    for quest_idx in range(len(question_list)):
+        temp_str = question_list[quest_idx]
+        for str_idx in range(len(temp_str)):
+            if temp_str[str_idx] == ".":
+                question_list[quest_idx] = temp_str[str_idx+1:].strip()
+                break
+
+    return question_list
