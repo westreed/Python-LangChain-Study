1a95f711a684ee421db6d8926338a6f1fd42ef1f
JangSeHun <westreed@naver.com>
2023-07-02 23:33:20 +0900

    feat: MVP스러운 예제 만들기
    Langchain으로 개발하려면, 관리해야하는 데이터들이 많다.
    DataManager로 묶어서 관리함.

---
+++ mvp/data_manager.py
@@ -0,0 +1,54 @@
+import os
+import sys
+from typing import *
+
+from langchain.memory import ConversationBufferMemory
+
+
+class DataManager:
+    def __init__(self):
+        self.company = None
+        self.job = None
+        self.requirement = None
+        self.coverletter = None
+
+    def set_data(self, input_data: Dict):
+        self.company = input_data["user_company"]
+        self.job = input_data["user_job"]
+        self.requirement = input_data["job_requirement"]
+        self.coverletter = input_data["cover_letter"]
+
+    def get_data(self) -> Dict:
+        return {
+            "company": self.company,
+            "job": self.job,
+            "requirement": self.requirement,
+            "coverletter": self.coverletter
+        }
+
+
+class MemoryManager:
+    memory: ConversationBufferMemory
+
+    def __init__(self):
+        self.memory = ConversationBufferMemory(
+            memory_key="chat_history",
+            return_messages=True
+        )
+
+    def get_memory(self) -> ConversationBufferMemory:
+        return self.memory
+
+
+class KeyManager:
+    def __init__(self):
+        self.openai_api_key = None
+        self.serpapi_api_key = None
+        if sys.platform == 'darwin':
+            # MacOS
+            self.openai_api_key = os.environ["OPENAI_API_KEY"]
+            self.serpapi_api_key = os.environ["SERPAPI_API_KEY"]
+        elif sys.platform == 'win32':
+            # Windows
+            self.openai_api_key = os.getenv("OPENAI_API_KEY")
+            self.serpapi_api_key = os.getenv("SERPAPI_API_KEY")
