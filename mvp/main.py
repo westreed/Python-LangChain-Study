b311d3dd971f0ecf4993da515ca6eb6580538007
JangSeHun <westreed@naver.com>
2023-07-03 19:01:29 +0900

    feat: MVP스러운 예제 만들기
    사용자 데이터 평가와 질문답변 평가 진행까지 완성됨.

--- mvp/main.py
+++ mvp/main.py
@@ -2,6 +2,8 @@ import util
 from pprint import pprint
 from data_manager import *
 from core.create_question import create_question
+from core.evaluate_input_info import evaluate_input_info
+from core.question_and_answer import question_and_answer
 
 
 def input_process(manager: DataManager):
@@ -14,11 +16,19 @@ def input_process(manager: DataManager):
 if __name__ == "__main__":
     key_manager = KeyManager()
     data_manager = DataManager()
-    memory_manager = MemoryManager()
+    evaluation_manager = EvaluationManager()
     input_process(data_manager)
     pprint(f"DataManager : {data_manager.get_data()}")
+    print("----------------------------------------")
 
-    question_list = create_question(data_manager, memory_manager, key_manager)
+    evaluate_input_info(data_manager, evaluation_manager)
+
+    print("----------------------------------------")
+
+    question_list = create_question(data_manager)
     question_manager = QuestionManager(question_list)
 
     print(question_list)
+    print("----------------------------------------")
+
+    question_and_answer(data_manager, question_manager)
